4c82fc0cf735706b97db7cfffdf1674cae8f3223
00003517 <00003517@AGI-LT-1601.arisglobal.com>
2021-09-23 15:42:01 +0530

    First commit

---
+++ src/main/java/com/Smitha/AriseMavenTestNG/Test001.java
@@ -0,0 +1,20 @@
+package com.Smitha.AriseMavenTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test001 {
+
+	public static void main(String[] args) throws InterruptedException {
+		 System.setProperty("webdriver.chrome.driver","C:\\SVN\\Automation\\Automation_Course\\chromedriver_win32\\chromedriver.exe");
+		 WebDriver myD=new ChromeDriver();
+		 myD.get("https://www.google.com/");
+		 myD.manage().window().maximize();
+		 String vTitle=myD.getTitle();
+		 System.out.println("Title of the screen is " +vTitle);
+		 Thread.sleep(5000);
+		 myD.close();
+
+	}
+
+}
